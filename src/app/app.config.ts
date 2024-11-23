cf0dadb1cb90f10dd83ff62ec9dd39cfe27fd5da
Federico <fedealtava1992@gmail.com>
2024-11-23 21:00:20 +0100

    Form added and routes and validators for sing up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'crud-ng18',
+        appId: '1:574070946902:web:f1eb388d25b1c3109d8ff7',
+        storageBucket: 'crud-ng18.firebasestorage.app',
+        apiKey: 'AIzaSyAuaMOxerRANRVgQEJUkqVj627-Vn2qhSA',
+        authDomain: 'crud-ng18.firebaseapp.com',
+        messagingSenderId: '574070946902',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { hasEmailError, isRequired } from '../../utils/validators';
+
+interface FormSingup {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-up.component.html',
+})
+export default class SignUpComponent {
+  private _formBuilder = inject(FormBuilder);
+
+  isRequired(field: 'email' | 'password') {
+    return isRequired(field, this.form);
+  }
+  hasEmailError() {
+    return hasEmailError(this.form);
+  }
+
+  form = this._formBuilder.group<FormSingup>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', Validators.required),
+  });
+
+  submit() {
+    if (this.form.invalid) return;
+    const { email, password } = this.form.value;
+    if (!email || !password) return;
+    console.log({ email, password });
+  }
+}
